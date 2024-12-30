5ce6d8783ec482ee4bced006f2771f388fad632f
Tristan Sim <tsym0098@gmail.com>
2024-12-30 18:13:22 +0800

    Created Structure for Motor

--- Control Systems/C++/Equipment/motor.h
+++ Control Systems/C++/Equipment/motor.h
@@ -8,12 +8,42 @@
 #ifndef MOTOR_H // Include Guard: Checks if Class is not already defined during Compilation
 #define MOTOR_H // Define: If Class is not Defined, Defines Class and prevents Re-Inclusion
 
+// Structure: SCADA Motor 
+// Operator Interface with the Motor Class
+struct ScadaMotor
+{  
+   // Feedback Variables: Reaed-Only Access
+   bool fb_RunStatus; 
+   bool fb_SelectorMode;
+   bool fb_TripStatus; 
+   
+   int fb_AlarmState;                     
+   unsigned int fb_RunHours; 
+   unsigned int fb_FailToStartCounter;
+   unsigned int fb_FailToStopCounter; 
+
+   // Control Variables: Read & Write Access
+   int ctrl_ControlMode; 
+   
+   unsigned int ctrl_FailToStartTimeLimit; 
+   unsigned int ctrl_FailToStopTimeLimit; 
+
+   // Control Variables: Simulate Inputs (Should Physical Inputs be Unavailable)
+   bool ctrl_SimulationMode; 
+   bool ctrl_SimulateRunStatus; 
+   bool ctrl_SimulateTripStatus; 
+   bool ctrl_SimulateSelectorMode; 
+};
+
 class Motor 
 {
    public:
       bool runStatus; 
-      bool selectorMode;      
       bool tripStatus; 
+      bool selectorMode;      
+
+   private: 
+      bool _runStatus; 
 };
 
 #endif
